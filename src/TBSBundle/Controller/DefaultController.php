211b4282276edd3305a62b10501805a431cbe884
balastephane <stephane_bala@yahoo.fr>
2016-01-05 20:27:37 +0100

    Update de getEntityManager à getManager

--- src/TBSBundle/Controller/DefaultController.php
+++ src/TBSBundle/Controller/DefaultController.php
@@ -8,18 +8,18 @@ use TBSBundle\Entity\User;
 class DefaultController extends Controller
 {
     public function indexAction(){
-    	$em = $this->getDoctrine()->getEntityManager();
+    	$em = $this->getDoctrine()->getManager();
         return $this->render('TBSBundle:Default:index.html.twig');
     }
 
      public function deleteAction(){
-     	$em = $this->getDoctrine()->getEntityManager();
+     	$em = $this->getDoctrine()->getManager();
      	$users = $em->getRepository("TBSBundle:User")->findAll();
      	return $this->render('TBSBundle:Default:delete.html.twig',array('users'=>$users));
      }
 
     public function deleteUserAction(User $user){
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager();
 
         $em->remove($user);
         $em->flush();
@@ -28,7 +28,7 @@ class DefaultController extends Controller
      }
 
      public function editUserAction(User $user){
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager();
         $form = $this->createForm(new UserType(), $user);
         $request = $this->getRequest();
         if($request->isMethod('POST')){
