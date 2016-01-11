51ad66f11b50d9afbea81acdd82b493a7028d07e
balastephane <stephane_bala@yahoo.fr>
2016-01-11 17:02:07 +0100

    Ajout de plusieurs commandes

--- src/TBSBundle/Controller/OrderlineController.php
+++ src/TBSBundle/Controller/OrderlineController.php
@@ -17,10 +17,11 @@ class OrderlineController extends Controller
         $o = new Orderline();
         $form2 = $this->createForm('TBSBundle\Form\OrderlineType', $o);
         $form2->handleRequest($request);
+        $em = $this->getDoctrine()->getManager();
 
-        if ($form2->isSubmitted() && $form2->isValid()) {
+        if(isset($_POST['final']))
+        {
             
-            $em = $this->getDoctrine()->getManager();
             $o->setBId($em->getRepository("TBSBundle:Basket")->find($basket->getBId()));
 
             $test = $o->getPId();
@@ -44,13 +45,48 @@ class OrderlineController extends Controller
             $em->persist($basket);
             $em->flush();   
 
-            // Si le bouton valider est appuyé
             return $this->redirect($this->generateUrl("tbs_index"));
+
+        }
+        if ($form2->isSubmitted() && $form2->isValid()) {    
+
+
+
+
+
+            $em = $this->getDoctrine()->getManager();
+            $o->setBId($em->getRepository("TBSBundle:Basket")->find($basket->getBId()));
+
+            $test = $o->getPId();
+
+            $product = $em->getRepository("TBSBundle:Product")->findOneByPId($test);
+
+            $stock = $em->getRepository("TBSBundle:Stock")->findOneBySId($product->getSId());
+
+            $new_stock = $stock->getSTotal() - ($o->getOlQtt() * $product->getPUnit());
+
+            $stock->setSTotal($new_stock);
+
+            $o->setTest($stock->getSId());
+
+            
+            $em->persist($o);
+            $em->persist($stock);
+
+            // Finalisation de la commande
+            //$basket->setBStatus('sent');
+            //$em->persist($basket);
+            $em->flush();   
+
+
+            $orderlines = $em->getRepository("TBSBundle:Orderline")->findByBId($basket->getBId());
+            // Si le bouton valider est appuyé
+            return $this->render('TBSBundle:Orderline:add2.html.twig',array('form2'=> $form2->createView(), 'basket'=> $basket ,'orderlines'=> $orderlines,));
             
             // Si le bouton nouvelle ligne est appuyé
             // return $this->redirect($this->generateUrl("tbs_add_orderline"));
         }
-       
+        
 
         return $this->render('TBSBundle:Orderline:add.html.twig',array('form2'=> $form2->createView(),));
     }
